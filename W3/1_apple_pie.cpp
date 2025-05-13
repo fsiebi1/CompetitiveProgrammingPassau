93ace391479d50168ede1c8bd2bb9d751863ec29
fsiebi1 <florian@raeucher.info>
2025-05-13 15:01:34 +0200

    versuch W3

---
+++ W3/1_apple_pie.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define MAX_N 2'000'000
+#define MAX_H 500'005
+
+using namespace std;
+
+int h, n;
+int arr[MAX_H];
+
+int main() {
+	int t;
+	cin >> t;
+
+    for(; t > 0; t--) {
+        cin >> h >> n;
+
+        for(int i = 0; i < h; i++)
+            scanf("%d", arr + i);
+
+        
+    }
+}
